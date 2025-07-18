011bd9de72656c3d437f902e856d1e213a96afd9
habeebfounderos <habeeb.founderos@gmail.com>
2025-07-18 12:19:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.floor_plan import floor_plan_bp
 
-app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
+app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'app', 'src', 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
 # Enable CORS for all routes
